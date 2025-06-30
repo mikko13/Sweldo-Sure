ea362e188d23167bfbda9443753ec222088baf68
mikko13 <mikko.samaniego.cics@ust.edu.ph>
2025-06-30 18:41:37 +0800

    Update CreatePayrollForm.jsx

--- client/src/pages/UserComponents/PayrollComponents/CreatePayrollComponents/CreatePayrollForm.jsx
+++ client/src/pages/UserComponents/PayrollComponents/CreatePayrollComponents/CreatePayrollForm.jsx
@@ -404,8 +404,6 @@ function CreatePayrollForm({ onSubmit }) {
                   type="number"
                   id="overtime"
                   name="overtime"
-                  min="0"
-                  step="0"
                   value={formData.overtime}
                   onChange={handleChange}
                   className="w-full rounded-md border border-green-200 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-green-500 transition-all duration-200"
